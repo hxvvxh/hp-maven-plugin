ef407857a0706967e339c4679cdd29cc7fcb1f87
hxvvxh <2732259311@qq.com>
2020-09-13 22:09:12 +0800

    1

--- hp-message-maven-plugin/src/main/java/message/Hpmessage.java
+++ hp-message-maven-plugin/src/main/java/message/Hpmessage.java
@@ -1,3 +1,5 @@
+package message;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
---
+++ hp-message/README.md
@@ -0,0 +1,6 @@
+# hp-maven-plugin
+
+## maven 插件学习
+
+    从github的特定仓库的特定分支的制定文件(CN_EN_message.properties) 获取国际化信息，
+    并将其加载到springboot环境中。
