89398806bae6edbf17554b31653d9fffae6573cc
hxvvxh <2732259311@qq.com>
2020-09-16 22:23:31 +0800

    12

--- hp-message-maven-plugin/src/main/java/message/Hpmessage.java
+++ hp-message-maven-plugin/src/main/java/message/Hpmessage.java
@@ -1,11 +1,23 @@
 package message;
 
+import com.alibaba.fastjson.JSON;
+import message.dto.MessageDto;
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,25 +29,87 @@ import java.util.Map;
 @Mojo(name = "Message")
 public class Hpmessage extends AbstractMojo {
 
-    private Map<String,String> enToCn=new HashMap<>();
+    private Map<String, String> enToCn = new HashMap<>();
 
     @Parameter(property = "Message.token")
     private String token;
-    @Parameter(property = "Message.branch",defaultValue = "master")
+    @Parameter(property = "Message.branch", defaultValue = "master")
     private String branch;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
 
-        if (null == token){
+        if (null == token) {
             getLog().error("Message token is null please check");
-        }
-        else{
+        } else {
             //根据token获取github上的仓库及分支信息
-            String url="https://api.github.com/repos/hxvvxh/hp-maven-plugin/contents/hp-message/CN_EN_message.properties?ref="+branch;
-            getLog().info("try to github api with url:"+url+"\\n"+" token:"+token);
+            String url = "https://api.github.com/repos/hxvvxh/hp-maven-plugin/contents/hp-message/CN_EN_message.properties?ref=" + branch;
+            getLog().info("try to github api with url:" + url + "\\n" + " token:" + token);
             //http调用，转换成JSON 获取content信息 转换成map
+            MessageDto dto=get(url);
 
             //调用springboot 国际化信息转换LocaleResolver
         }
+    }
+
+    public MessageDto get(String url) {
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        // 创建Get请求
+        HttpGet httpGet = new HttpGet(url);
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 配置信息
+            RequestConfig requestConfig = RequestConfig.custom()
+                    // 设置连接超时时间(单位毫秒)
+                    .setConnectTimeout(5000)
+                    // 设置请求超时时间(单位毫秒)
+                    .setConnectionRequestTimeout(5000)
+                    // socket读写超时时间(单位毫秒)
+                    .setSocketTimeout(5000)
+                    // 设置是否允许重定向(默认为true)
+                    .setRedirectsEnabled(true).build();
+
+            // 将上面的配置信息 运用到这个Get请求里
+            httpGet.setConfig(requestConfig);
+            httpGet.setHeader("Authorization", token);
+            httpGet.setHeader("Accept", "application/vnd.github.v3+json");
+
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                String str=EntityUtils.toString(responseEntity);
+                System.out.println("响应内容为:" + str);
+                JSON.parse(str);
+                MessageDto dto= JSON.parseObject(str, MessageDto.class);
+                getLog().info("MessageDto :"+dto.toString());
+                return dto;
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 }
--- hp-message-maven-plugin/src/test/java/test.java
+++ hp-message-maven-plugin/src/test/java/test.java
@@ -1,10 +1,4 @@
-import jdk.nashorn.internal.parser.JSONParser;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Base64;
 
 /**
@@ -17,41 +11,11 @@ public class test {
     public static void main(String[] args) {
         Base64.Decoder base64 = Base64.getDecoder();
         try {
-            String str = new String(base64.decode(""), "UTF-8");
+            String s="aHAudGVzdD3mtYvor5UKaHAudGVzdDI95rWL6K+VMgo=\n";
+            s=s.replaceAll("\r|\n","");
+            String str = new String(base64.decode(s), "UTF-8");
             System.out.println(str);
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
-
-        String url = "https://api.github.com/repos/hxvvxh/hp-maven-plugin/contents/hp-message/CN_EN_message.properties?ref=master";
-        try {
-            URL uri = new URL(url);
-            URLConnection conn = uri.openConnection();
-            conn.setRequestProperty("Authorization", "2466dc723708d0ff74dbe779dd29c70a0be01272");
-            conn.setRequestProperty("Content-Type", "application/Json; charset=UTF-8");
-            conn.setRequestProperty("Accept", "application/vnd.github.v3+json");
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            conn.setConnectTimeout(5000);
-            conn.connect();
-            //服务器返回东西了，先对响应码判断
-            StringBuffer result =new StringBuffer();
-            BufferedReader in = null;
-            // 定义 BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(
-                    conn.getInputStream(), "UTF-8"));
-            String line;
-            while ((line = in.readLine()) != null) {
-                if (null != line){
-                    result.append(line);
-                }
-            }
-            System.out.println(JSONParser.quote(result.toString()));
-
-        } catch (Exception e) {
             e.printStackTrace();
         }
     }
